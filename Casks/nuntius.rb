f6a07e3986f024518c61c822cc5fbf75c0f0e7ed
Magnus Biolzi Hansen <magnus@biolzi.com>
2023-06-17 21:44:03 +0200

    Updated Nuntius to v1.0.0-beta.2

--- Casks/nuntius.rb
+++ Casks/nuntius.rb
@@ -1,8 +1,8 @@
 cask "nuntius" do
-    version "1.0.0-beta.1"
-    sha256 "00e023476e8d786f49bd4d0843c5e4e9f51a0971eeee5da94a788e9abc713618"
+    version "1.0.0-beta.2"
+    sha256 "d768d90167a49ad31f6e451ef96448337a183e611b4e7ccbbd1db8d23b9198f1"
 
-    url "https://github.com/nuntius-app/nuntius-app-releases/releases/download/v#{version}/Nuntius-1.0.0.dmg"
+    url "https://github.com/Nuntius-App/nuntius-app-releases/releases/download/v#{version}/Nuntius-v#{version}-arm64.dmg"
     name "Nuntius"
     desc "The development tool for communicating with RabbitMQ"
     homepage "https://nuntius.app"
