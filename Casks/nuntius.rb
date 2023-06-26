dd0a006ec24d84a25d52f2e3118254d0575df6b2
Magnus Biolzi Hansen <magnus@biolzi.com>
2023-06-26 10:04:26 +0200

    Updated Nuntius sha

--- Casks/nuntius.rb
+++ Casks/nuntius.rb
@@ -1,6 +1,6 @@
 cask "nuntius" do
     version "1.0.0-beta.2"
-    sha256 "d768d90167a49ad31f6e451ef96448337a183e611b4e7ccbbd1db8d23b9198f1"
+    sha256 "55a6203b3a78ddd2b55c98c8170854835ce8d6a78a7ec28397ad0ded0a4a3599"
 
     url "https://github.com/Nuntius-App/nuntius-app-releases/releases/download/v#{version}/Nuntius-#{version}-arm64.dmg"
     name "Nuntius"
